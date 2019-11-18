6e222827df09f290bc2aa71fdbdab33ba126026c
Richu Mathew <richu.a.mathew@my.occc.edu>
2019-11-17 22:41:33 -0600

    90% of initial design of all except show reservation done.

---
+++ View/mapsApi.js
@@ -0,0 +1,3 @@
+// https://stackoverflow.com/questions/30883736/how-to-create-sub-string-in-a-line-read-from-text-file-separated-by-comma?
+// https://medium.com/risan/track-users-location-and-display-it-on-google-maps-41d1f850786e
+
